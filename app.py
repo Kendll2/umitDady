cfe3e5537f0c6eeb5159d567c8b56f5ae510cc17
Kendll2 <nellzeta3@gmail.com>
2026-05-29 19:49:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ def index():
         )
 
     if live_id:
-        stream_url = f"https://2i4.d72577a9dd0ec71.cfd/-{live_id}.php"
-        headers = {**HEADERS_TV, "referer": f"https://2i4.d72577a9dd0ec71.cfd.php?id={live_id}"}
+        stream_url = f"https://2i4.d72577a9dd0ec71.cfd/b2/mono.m3u8.php"
+        headers = {**HEADERS_TV, "referer": f"https://2i4.d72577a9dd0ec71.cfd/b2/mono.m3u8.php?id={live_id}"}
 
         r1 = SESSION.get(stream_url, headers=headers, timeout=15)
         site = r1.text
