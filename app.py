887a0162ce35f069f51c78ce59a1972a93219393
Kendll2 <nellzeta3@gmail.com>
2026-05-29 19:48:20 +0300

    Fix referer URL format in headers

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def index():
 
     if live_id:
         stream_url = f"https://2i4.d72577a9dd0ec71.cfd/-{live_id}.php"
-        headers = {**HEADERS_TV, "referer": f"https://2i4.d72577a9dd0ec71.cfd/watch.php?id={live_id}"}
+        headers = {**HEADERS_TV, "referer": f"https://2i4.d72577a9dd0ec71.cfd.php?id={live_id}"}
 
         r1 = SESSION.get(stream_url, headers=headers, timeout=15)
         site = r1.text
