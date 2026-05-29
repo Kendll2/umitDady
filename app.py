ade784da3d0043afffa6fcfc14d0e325db2021d8
Kendll2 <nellzeta3@gmail.com>
2026-05-29 21:18:28 +0300

    Update stream URL and headers for live_id

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ def index():
         )
 
     if live_id:
-        stream_url = f"https://inattv1309.xyz/channel.html?id=b5"
-        headers = {**HEADERS_TV, "referer": f"https://inattv1309.xyz/channel.html?id=b5"}
+        stream_url = f"https://dlhd.pk/stream/stream-{live_id}.php"
+        headers = {**HEADERS_TV, "referer": f"https://dlhd.pk/watch.php?id={live_id}"}
 
         r1 = SESSION.get(stream_url, headers=headers, timeout=15)
         site = r1.text
